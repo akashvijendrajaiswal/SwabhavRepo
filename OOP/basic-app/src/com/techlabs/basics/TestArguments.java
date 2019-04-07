25522219ab6d53a1b1f11a398510a4cb7c9992c0
Akash <akashvijendrajaiswal@gmail.com>
2019-04-07 17:12:44 +0530

    first class after it

--- OOP/basic-app/src/com/techlabs/basics/TestArguments.java
+++ OOP/basic-app/src/com/techlabs/basics/TestArguments.java
@@ -3,10 +3,19 @@ package com.techlabs.basics;
 public class TestArguments {
 
 	public static void main(String[] args) {
+		int length;
+		length = args.length;
 		System.out.println(args.length);
-		System.out.println(args[0]);
+		
+		if(length>0)
+		{
 		String s = args[0];
-		System.out.println(s.length());
+		System.out.println("hello "+s);
+		}
+		else
+		{
+			System.out.println("thanks");
+		}
 
 	}
 
